4ccd6b02e8f268f6af1d39152d3c991bf78b1666
sasi <sasik71788@gmail.com>
2026-07-02 22:47:03 +0530

    Bookdetails

---
+++ bookdetails.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class book details{
+
+public void main static(String [] args){
+
+Scanner s= new Scanner(System.in);
+System.out.println("Enter the Book name:");
+String bookname=s.nextLine();
+System.out.println("Enter the author name:");
+String authorname=s.nextLine();
+System.out.println("Enter the book price:");
+int price=s.nextInt();
+System.out.println("Book name:"+bookname);
+System.out.println("Author name"+authorname );
+System.out.println("Book price:"+price);
+}
+}
+
+
+
